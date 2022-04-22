8ddad4c6e6b23b4e73d64b23a3341e240f013047
hungrybeagle <cyang@deltalearns.ca>
2022-04-22 08:46:17 -0700

    Update inputCmd.py

--- inputCmd.py
+++ inputCmd.py
@@ -1,17 +1,18 @@
 #!python3
 
 def inputIntV1(question):
-  """
-  input parameters: 
-  string question - this is the question that you will ask people when you prompt for input
-  """
-  while True:
-      inp = input(question)
-      try:
-          result = int(inp)
-      except Exception as e:
-          print("invalid entry for an interger")
-  return result
+    """
+    input parameters: 
+    string question - this is the question that you will ask people when you prompt for input
+    """
+    while True:
+        inp = input(question)
+        try:
+            result = int(inp)
+            break
+        except Exception as e:
+            print("invalid entry for an interger")
+    return result
           
 def inputIntV2(question):
   """
@@ -19,18 +20,17 @@ def inputIntV2(question):
     string question - this is the question that you will ask people when you prompt for input
   """
   while True:
-      inp = input(question)
-        for i in inp:
-          if i not in ["1234567890"]:
+    inp = input(question)
+    for i in inp:
+        valid = "1234567890"
+        if valid.find(i):
             print("invalid entry for an integer")
             break
-        else:
-          result = int(inp)
-          return result
+    else:
+        result = int(inp)
+        return result
  
 
-x = inputIntV1("Enter an integer")
-print(f"The integer is {x}")
-x = inputIntV2("Enter another integer")
-print(f"The second integer is {x}")
-    
+
+x = inputIntV2("Enter an integer")
+x = int(x)
